22c9ce27313303b12bfbebfb01341cd2e1a55fe4
Matthew Lee <mlee212@wch136-09.cs.ucr.edu>
2020-04-14 18:56:12 -0700

    Completed part 4

---
+++ Lab3_bitManipulation/turnin/mlee212_lab3_part4.c
@@ -0,0 +1,27 @@
+/*	Author: mlee212
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	//button = PINA & 0x01;
+	PORTB = PINA >> 4;
+	PORTC = PINA << 4;
+    }
+    return 1;
+}
